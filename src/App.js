5bcd333fc54f85a0fea14af2628cef4813199c74
huynet <cocwedc@gmail.com>
2019-05-05 00:27:41 -0500

    scss

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.scss'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="app-layout">
+            <div className="teams box">Settings</div>
+            <div className="channels box">
+                Decks
+                <li className="channels item">
+                    Today
+                </li>
+                <li className="channels item">
+                    Tomorrow
+                </li>
+                <li className="channels item">
+                    Month
+                </li>
+            </div>
+            <div className="header box">Paths</div>
+            <div className="messages box">
+                <ul className="message-list">
+                    <li />
+                    <li />
+                    <li />
+                    <li />
+                    <li />
+                </ul>
+            </div>
+            <div className="input box">
+                <input
+                    type="text"
+                    placeholder="Personal Goals?"
+                />
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
