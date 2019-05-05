4e65dba9c49d263b3a8768857be068ff4837deab
huynet <cocwedc@gmail.com>
2019-05-05 11:02:46 -0500

    basic todo

--- src/App.js
+++ src/App.js
@@ -7,22 +7,29 @@ import '../src/app/fonts/Font.scss'
 // Class Component: class App extends Component
 
 class App extends Component {
-
     state = {
         channel1: 'Today',
         channel2: 'Tomorrow',
         channel3: 'This Week',
         user: 'Huy',
-        todos: ['Xin chao', 'Afghanimals']
+        todos: ['Xin chao', 'Afghanimals'],
     }
 
-    switchHeader = (channel) => {
+    switchHeader = channel => {
         console.log(channel)
-        this.setState({paths: channel})
+        this.setState({ paths: channel })
+    }
+
+    handleChange = (event) => {
+        this.setState({ value: event.target.value })
+    }
+
+    handleSubmit = (event) => {
+        alert('A name was submitted: ' + this.state.value)
+        event.preventDefault()
     }
 
     render() {
-
         const { channel1, channel2, channel3, paths, user, todos } = this.state
 
         return (
@@ -30,13 +37,22 @@ class App extends Component {
                 <div className="teams box">{user}</div>
                 <div className="channels box">
                     Decks
-                    <button onClick={() => this.switchHeader(channel1)} className="channels item">
+                    <button
+                        onClick={() => this.switchHeader(channel1)}
+                        className="channels item"
+                    >
                         {channel1}
                     </button>
-                    <button onClick={() => this.switchHeader(channel2)} className="channels item">
+                    <button
+                        onClick={() => this.switchHeader(channel2)}
+                        className="channels item"
+                    >
                         {channel2}
                     </button>
-                    <button onClick={() => this.switchHeader(channel3)} className="channels item">
+                    <button
+                        onClick={() => this.switchHeader(channel3)}
+                        className="channels item"
+                    >
                         {channel3}
                     </button>
                 </div>
@@ -44,16 +60,19 @@ class App extends Component {
                 <div className="messages box">
                     <ul className="message-list">
                         {todos.map((todo, index) => {
-                            return (
-                                <li key={index}>
-                                    {todo}
-                                </li>
-                            )
+                            return <li key={index}>{todo}</li>
                         })}
                     </ul>
                 </div>
                 <div className="input box">
-                    <input type="text" placeholder="Personal Goals?" />
+                    <form onSubmit={this.handleSubmit}>
+                        <input
+                            type="text"
+                            value={this.state.value}
+                            onChange={this.handleChange}
+                        />
+                        <input type="submit" value="Submit" />
+                    </form>
                 </div>
             </div>
         )
