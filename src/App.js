598d15748d1a82434811b9c715797cd8a82c0b1a
huynet <cocwedc@gmail.com>
2019-05-05 10:58:06 -0500

    basic state wandering

--- src/App.js
+++ src/App.js
@@ -1,41 +1,63 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.scss'
+import React, { Component } from 'react'
 
-function App() {
-    return (
-        <div className="app-layout">
-            <div className="teams box">Settings</div>
-            <div className="channels box">
-                Decks
-                <li className="channels item">
-                    Today
-                </li>
-                <li className="channels item">
-                    Tomorrow
-                </li>
-                <li className="channels item">
-                    Month
-                </li>
+import './App.scss'
+import '../src/app/fonts/Font.scss'
+
+// Functional Component: function App()
+// Class Component: class App extends Component
+
+class App extends Component {
+
+    state = {
+        channel1: 'Today',
+        channel2: 'Tomorrow',
+        channel3: 'This Week',
+        user: 'Huy',
+        todos: ['Xin chao', 'Afghanimals']
+    }
+
+    switchHeader = (channel) => {
+        console.log(channel)
+        this.setState({paths: channel})
+    }
+
+    render() {
+
+        const { channel1, channel2, channel3, paths, user, todos } = this.state
+
+        return (
+            <div className="app-layout">
+                <div className="teams box">{user}</div>
+                <div className="channels box">
+                    Decks
+                    <button onClick={() => this.switchHeader(channel1)} className="channels item">
+                        {channel1}
+                    </button>
+                    <button onClick={() => this.switchHeader(channel2)} className="channels item">
+                        {channel2}
+                    </button>
+                    <button onClick={() => this.switchHeader(channel3)} className="channels item">
+                        {channel3}
+                    </button>
+                </div>
+                <div className="header box">{paths || user}</div>
+                <div className="messages box">
+                    <ul className="message-list">
+                        {todos.map((todo, index) => {
+                            return (
+                                <li key={index}>
+                                    {todo}
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </div>
+                <div className="input box">
+                    <input type="text" placeholder="Personal Goals?" />
+                </div>
             </div>
-            <div className="header box">Paths</div>
-            <div className="messages box">
-                <ul className="message-list">
-                    <li />
-                    <li />
-                    <li />
-                    <li />
-                    <li />
-                </ul>
-            </div>
-            <div className="input box">
-                <input
-                    type="text"
-                    placeholder="Personal Goals?"
-                />
-            </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default App
